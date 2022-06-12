7026bc225f139cdf3293fe72e8efcac5e5fab802
Sjiels <grennard.ttv@gmail.com>
2022-06-12 15:04:07 +0200

    v2.0

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const discord = require("discord.js");
-require("dotenv").config
+
+const Discord = require("discord.js")
+require("dotenv").config()
 
 
-
-const client = new discord.Client({
+const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
-});
+})
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
+    console.log(`Logged in as ${client.user.tag}`)
 })
 
 client.on("messageCreate", (message) => {
-    if(message.content == "hi")
-        message.reply("Hello World")
+    if (message.content == "hi"){
+        message.reply("Hello World!")
+    }
+})
+
+
+const welcomeChannelID = "916432290484191273"
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChannelID).send(`<@${member.id}> Welcome to the server!`)
 })
 
 client.login(process.env.TOKEN)
